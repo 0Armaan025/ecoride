2981b42b2bc587ecce95d2aef084bf4b732ac29a
Varun Gupta <varungupta@Varuns-MacBook-Pro.local>
2023-09-23 15:25:13 +0530

    added some feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dotenv/dotenv.dart';
 import 'package:ecoride/features/auth/loginScreen.dart';
 import 'package:ecoride/features/cargo_shipping/screens/add_cargo_screen.dart';
 import 'package:ecoride/features/home/screens/home_screen.dart';
@@ -5,6 +6,7 @@ import 'package:ecoride/features/notifications/screens/notification_screen.dart'
 import 'package:ecoride/features/public_transport/AskFromWhereToWhere.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
